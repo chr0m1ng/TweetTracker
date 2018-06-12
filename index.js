164aa9a281464682f0f47d48f2f74da97a44ddda
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-06-12 17:48:09 -0300

    index to git

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const request = require('request');
 const config = require('./config/keys.js');
 const lana = require('./config/telegram-config');
 const followList = require('./config/follow-list');
-const client = new Twitter(config);
 
-console.log(client);
+const client = new Twitter(config);
 
 client.stream('statuses/filter', {follow: followList.followListString}, (stream) => {
     stream.on('data', (tweet) => {
