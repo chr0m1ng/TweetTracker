4603ed1d385e55c168e41acb07d53c0ebef8c432
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-06-13 10:58:48 -0300

    Added Markdown

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ client.stream('statuses/filter', {follow: followList.followListString}, (stream)
     stream.on('data', (tweet) => {
         let dev = followList.followListId.find(x => x.id == tweet.user.id);
         if(dev != undefined) {
-            let message = `Tweet de ${dev.name}:\n${tweet.text}`;
-            request.post(lana.endpoint, { json : { 'chatId' : lana.chatId, 'message' : message } }, (err, resp) => {
+            let message = `*Tweet de ${dev.name}*\n${tweet.text}`;
+            request.post(lana.endpoint, { json : { 'chatId' : lana.chatId, 'message' : message, parse_mode: 'Markdown' } }, (err, resp) => {
                 if(err) {
                     console.log(err);
                 }
